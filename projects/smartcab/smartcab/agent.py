bbecbba9bc84257bfb686221180cef1a3b3242e4
Roshan Choxi <roshan.choxi@gmail.com>
2016-10-05 14:35:28 -0700

    implement policy based on q_values

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -3,37 +3,56 @@ from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 
+import random
+
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
     def __init__(self, env):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
-        self.color = 'red'  # override color
-        self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        # TODO: Initialize any additional variables here
+        self.color      = 'red'  # override color
+        self.planner    = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+        self.q_values   = {}
+        self.epsilon    = 0.5
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
 
     def update(self, t):
-        # Gather inputs
-        self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
-        inputs = self.env.sense(self)
-        deadline = self.env.get_deadline(self)
+        possible_actions = [None, "forward", "left", "right"]
 
-        # TODO: Update state
-        
-        # TODO: Select action according to your policy
-        action = None
+        # Gather inputs
+        self.next_waypoint  = self.planner.next_waypoint()  # from route planner, also displayed by simulator
+        inputs              = self.env.sense(self)
+        deadline            = self.env.get_deadline(self)
+
+        self.state = (inputs["light"], inputs["oncoming"], deadline)
+
+        # Select action according to your policy
+        best_action  = None
+        best_q_value = self.q_values.get((self.state, best_action), 0)
+
+        for action in possible_actions:
+            q_value = self.q_values.get((self.state, action), 0)
+            if q_value > best_q_value:
+                best_action  = action
+                best_q_value = q_value
+
+        if random.random() < self.epsilon:
+            next_action = best_action
+        else:
+            print "random choice"
+            other_actions = filter(lambda x: x != best_action, possible_actions)
+            next_action = random.choice(other_actions)
 
         # Execute action and get reward
-        reward = self.env.act(self, action)
+        reward = self.env.act(self, next_action)
 
         # TODO: Learn policy based on state, action, reward
-
-        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
-
+        print "LearningAgent.update(): state = {}".format(self.state)
+        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, next_action, reward)  # [debug]
+        import pdb; pdb.set_trace()
 
 def run():
     """Run the agent for a finite number of trials."""
